060ad735c5adf9fc2b8482f677895dc65fc290ac
Armel Soro <asoro@redhat.com>
2025-06-06 17:46:45 +0200

    wip - describe operator and operand pods

--- tests/e2e/e2e_suite_test.go
+++ tests/e2e/e2e_suite_test.go
@@ -213,10 +213,13 @@ func verifyControllerUp(g Gomega, managerPodLabel string) {
 }
 
 func getPodLogs(ns string, label string) string {
-	cmd := exec.Command(helper.GetPlatformTool(), "logs",
-		"-l", label,
-		"-n", ns,
-	)
+	cmd := exec.Command(helper.GetPlatformTool(), "logs", "-l", label, "-n", ns)
+	output, _ := helper.Run(cmd)
+	return string(output)
+}
+
+func describePod(ns string, label string) string {
+	cmd := exec.Command(helper.GetPlatformTool(), "describe", "pods", "-l", label, "-n", ns)
 	output, _ := helper.Run(cmd)
 	return string(output)
 }
@@ -243,7 +246,17 @@ func fetchOperandLogs(ns string, crLabel string, raw bool) func() string {
 
 func fetchOperatorAndOperandLogs(managerPodLabel string, ns string, crLabel string) func() string {
 	return func() string {
-		return fmt.Sprintf("%s\n\n%s\n", fetchOperatorLogs(managerPodLabel, false)(), fetchOperandLogs(ns, crLabel, false)())
+		return fmt.Sprintf("%s\n\n%s\n",
+			fetchOperatorLogs(managerPodLabel, false)(),
+			fetchOperandLogs(ns, crLabel, false)())
+	}
+}
+
+func describeOperatorAndOperatorPods(managerPodLabel string, ns string, crLabel string) func() string {
+	return func() string {
+		return fmt.Sprintf("%s\n\n%s\n",
+			describePod(_namespace, managerPodLabel),
+			describePod(ns, crLabel))
 	}
 }
 
